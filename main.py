84d524a409cd3cee4749b43c20d587362a35886e
dasiksaol <2023218ol@aupp.edu.kh>
2023-08-21 12:26:28 +0700

    new

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import random
+import os
+import time
+
+def clear_terminal():
+    if os.name == 'nt':  
+        _ = os.system('cls')
 
 def info():
     name = input("Please Input Username: ")
@@ -14,23 +20,44 @@ def hit(player):
     addition = random.randint(1,13)
     player += addition
     return player
-        
+       
+def stand(player, bot, name):
+    if player > bot:
+        print(f"Player: {player} > House: {bot}")
+        print(f"Congratulation {name}! You Won")
+    elif player < bot:
+        print(f"Player: {player} < House: {bot}")
+        print(f"You Lose, {name}!!")
+    elif player == bot:
+        print(f"Player: {player} = House: {bot}")
+        print(f"it's Draw, {name}!!")
+
 def main():
     name = info()
     player, bot = start()
-    print(player)
+    clear_terminal()
+    print(f"player: {player}")
     
-    while True:
-        hit_ = input("Hit?")
+    while player <= 21:
+    
+        print("Do you wish to Hit or Stand. Type 1 for Hit. Type 0 for Stand")
+        hit_ = input("Hit or Stand? \n>>> ")
         try:
             hit_ = int(hit_)
-            if hit_ == 1 and player <= 21:
-                hit_number = hit(player)
-                print(hit_number)
-            elif player > 21:
-                print(f"{player}, You Lose")
+        
+            time.sleep(1)
+            clear_terminal()
+            if hit_ == 1:
+                player = hit(player)
+                if player < 21:
+                    print(f"player: {player}")
+                elif player > 21:
+                    print(f"\nPlayer: {player} \nYou Lose, {name}. You Went Over 21")
+                    break
+            elif hit_ == 0:
+                player = stand(player, bot, name)
                 break
-
+            
         except ValueError:
             print("Please Enter 1 (hit) or 0 (stand)")
             
